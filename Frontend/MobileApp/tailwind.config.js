9ed98f51cf0b2553ce002fc2504754d3497e4756
Phạm Đăng Minh Khang <phamdangminhkhang2005@gmail.com>
2026-09-23 01:47:57 +0700

    update config nativewind

---
+++ Frontend/MobileApp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/screen/**/*.{js,jsx,ts,tsx}',
+    './src/store/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
